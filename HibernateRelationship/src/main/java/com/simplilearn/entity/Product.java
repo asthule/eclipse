323e1de8ef9a76a863a6567acba331a084fcd771
asthule <119798447+asthule@users.noreply.github.com>
2023-01-26 23:05:59 +0530

    Hibernate

---
+++ HibernateRelationship/src/main/java/com/simplilearn/entity/Product.java
@@ -0,0 +1,34 @@
+package com.simplilearn.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Product {
+
+	private int id;
+	private String name;
+	private Double price;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	
+	
+}
