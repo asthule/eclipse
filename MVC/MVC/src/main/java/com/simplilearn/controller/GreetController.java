87713a09f8e1c0c209b0c7ba119a380d6409adb4
asthule <119798447+asthule@users.noreply.github.com>
2023-03-19 19:48:06 +0530

    MVC

---
+++ MVC/MVC/src/main/java/com/simplilearn/controller/GreetController.java
@@ -0,0 +1,16 @@
+package com.simplilearn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetController {
+
+	@GetMapping("/greet")
+	public String greet() {
+		
+		 
+		
+		return "hello";
+	}
+}
